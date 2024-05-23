d1b14bbf2505c45e044955faa78daffaeb3ee78c
XI0val <114651543+XI0val@users.noreply.github.com>
2024-05-23 09:01:54 +0200

    Update menu-criador.php mps

--- vista/criador/menu-criador.php
+++ vista/criador/menu-criador.php
@@ -9,7 +9,7 @@
 ?>
 
 <script async
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDE5zSl0JCMktXTnLPpJ45uFPg_XNHWTRU&callback=initMap">
+   
 </script>
 <script>
     let map;
@@ -33,11 +33,7 @@
 </script>
 
 <script>
-  (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
-    key: "AIzaSyDE5zSl0JCMktXTnLPpJ45uFPg_XNHWTRU",
-    // Add other bootstrap parameters as needed, using camel case.
-    // Use the 'v' parameter to indicate the version to load (alpha, beta, weekly, etc.)
-  });
+  
 </script>
 
 <style type="text/css">
